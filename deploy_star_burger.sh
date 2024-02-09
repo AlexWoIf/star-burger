ef5a57679aa395e71fd72720016f8e5597982765
AlexWoIf <alexey.barannikov@gmail.com>
2024-02-09 13:56:16 +0300

    upgrade deplay script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
+set -ex
+
 git pull
+pip install -r requirements.txt
+npm ci
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+systemctl restart star_burger_django
