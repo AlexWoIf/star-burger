9b5636abc8aaf24efdc65dcf8f942271ea52a910
AlexWoIf <alexey.barannikov@gmail.com>
2024-02-09 16:53:31 +0300

    notify rollbar about new deploy

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -ex
+set -e
 
 git pull
 pip install -r requirements.txt
@@ -9,3 +9,20 @@ npm ci
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 systemctl restart star_burger_django
+
+. .env
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header 'X-Rollbar-Access-Token: $ROLLBAR_TOKEN' \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "environment": "$ROLLBAR_TOKEN",
+  "revision": "$(git rev-parse HEAD)",
+  "comment": "deploy script",
+  "local_username": "$USER"
+}
+'
+
+echo "Deploy successful!!!"
