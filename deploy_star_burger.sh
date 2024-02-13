0751a70db6bc7b7fd9e938868f1f4d7cb348e206
AlexWoIf <alexey.barannikov@gmail.com>
2024-02-14 01:21:07 +0300

    add permissions to restart services in deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -8,7 +8,7 @@ npm ci
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-systemctl restart star_burger_django
+sudo systemctl restart star_burger_django
 
 . .env
 curl --request POST \
