1b16116de0b60f3dd0d1fe723066b74aa2e7fa63
Alexey Barannikov <alexey.barannikov@gmail.com>
2024-05-19 22:11:52 +0300

    fix deployment script

--- deploy_scripts/production_myserver/deploy_docker.sh
+++ deploy_scripts/production_myserver/deploy_docker.sh
@@ -3,8 +3,8 @@
 docker compose up -d
 mkdir -p media
 mkdir -p static
-mount --bind /var/lib/docker/volumes/star-burger_static/_data/ /opt/star-burger/static/
-mount --bind /var/lib/docker/volumes/star-burger_media/_data/ /opt/star-burger/media/
+mount --bind /var/lib/docker/volumes/production_myserver_static/_data/ /opt/star-burger/static/
+mount --bind /var/lib/docker/volumes/production_myserver_media/_data/ /opt/star-burger/media/
 cp starburger_docker.conf /etc/nginx/sites-available
 ln -sf /etc/nginx/sites-available/starburger_docker.conf /etc/nginx/sites-enabled
 nginx -s reload
